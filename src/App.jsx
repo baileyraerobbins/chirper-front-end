1e5f7a971eb42874613baa249c12ad69696b9452
baileyraerobbins <baileyrae.robbins@gmail.com>
2021-03-18 23:52:10 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 import Chirp from './components/Chirp'
 
 
-const App = () => { //create react hook
+const App = () => { 
 
     const initialList = [ //array of initial messages
         {
