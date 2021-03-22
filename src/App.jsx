033cea0c6dc26c609a605ea43b0911078256ca5b
baileyraerobbins <baileyrae.robbins@gmail.com>
2021-03-22 12:39:06 -0400

    adding new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,47 @@
 import React, { useState } from 'react'; //import react, useState
 import { v4 as uuidv4 } from 'uuid'; //get unique ids
 import moment from 'moment';
-import Chirp from './components/Chirp'
+import Chirp from './components/Chirp';
+import Form from './components/Form'
+
 
 
 const App = () => { 
 
     const initialList = [ //array of initial messages
         {
-            name: 'Winnie the Pooh',
-            chirp: 'Hi! Has anyone seen my honey?',
+            name: 'Tigger',
+            chirp: 'Let me ask around!',
             id: uuidv4(),
+            time: moment().format('dddd, MMMM Do YYYY, h:mm a')
         },
         {
             name: 'Piglet',
             chirp: 'No, sorry! It is a bit blustery today. Maybe it blew away.',
             id: uuidv4(),
+            time: moment().format('dddd, MMMM Do YYYY, h:mm a')
         },
         {
-            name: 'Tigger',
-            chirp: 'Let me ask around!',
+            name: 'Winnie the Pooh',
+            chirp: 'Hi! Has anyone seen my honey?',
             id: uuidv4(),
+            time: moment().format('dddd, MMMM Do YYYY, h:mm a')
+            
         },
 
     ];
-    const [name, setName] = useState(''); //use state for name
-    const [chirp, setChirp] = useState(''); //use state for chirp messages
+
     const [arr, setArr] = useState(initialList); //use state for initial list
 
-    const handleClick = () => { //handle button click
-        const newList = arr.concat({ name, chirp, id: uuidv4(), time: moment().format() }) //button adds new name, chirp and id to array
-        setArr(newList);
+    const handleClick = (name, chirp) => { //handle button click
+        const newChirp ={
+            name,
+            chirp,
+            id:uuidv4(),
+            time:moment().format('dddd, MMMM Do YYYY, h:mm a')
+        }
+        const newList = arr.concat() //button adds new name, chirp and id to array
+        setArr([newChirp, ...arr]);
     };
 
     const submitHandler = e => { //prevent submit button from refreshing data
@@ -42,33 +53,11 @@ const App = () => {
         <>
             <div className='container'>
                 <div className='row'>
-                    <div className='chirpForm shadow p-3 mb-5 rounded col-6'>
-                        <strong>Send a Message!</strong>
-                        <form onSubmit={submitHandler}>
-                            <div className='form-group'>
-                                <input
-                                    className='form-control'
-                                    placeholder='Enter your name...'
-                                    value={name}
-                                    onChange={e => setName(e.target.value)} />
-                            </div>
 
-                            <div className='form-group'>
-                                <textarea
-                                    className='form-control'
-                                    maxlength="150"
-                                    rows='4'
-                                    placeholder='Chirp your message (in 150 characters or less)...'
-                                    value={chirp} onChange={e => setChirp(e.target.value)} />
-                            </div>
-                            <div>
-                                <button className='btn btn-primary' onClick={handleClick}> Chirp </button>
-                            </div>
-                        </form>
-                    </div>
+                    <Form stopSubmit={submitHandler} addChirp={handleClick} />
 
                     <div className='msgBox col-6'>
-                        {arr.map(props => <Chirp name={props.name} chirp={props.chirp}/>)}
+                        {arr.map(singleChirp => <Chirp key={singleChirp.id} name={singleChirp.name} chirp={singleChirp.chirp} time={singleChirp.time}/>)}
                     </div>
                 </div>
             </div>
@@ -78,6 +67,8 @@ const App = () => {
 
 export default App;
 
+
+//IGNORE: old code, before breaking into smaller components and adding new changes 
 // import React, { useState } from 'react'; //import react, useState
 // import { v4 as uuidv4 } from 'uuid'; //get unique ids
 // import moment from 'moment';
